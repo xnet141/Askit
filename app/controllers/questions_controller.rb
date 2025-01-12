30d5e306b1f7815241749f6ca5e2f7de957dc59d
Alex Erofeev <xnet141@gmail.com>
2025-01-12 14:42:19 +0300

    My commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,55 @@
+class QuestionsController < ApplicationController
+  before_action :set_question!, only: [:show, :edit, :update, :destroy]
+
+  def show
+    @answer = @question.answers.build
+    @answers = @question.answers.order created_at: :desc
+  end
+
+  def destroy
+    @question.destroy
+    flash[:success] = 'Question deleted!'
+    redirect_to questions_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update questions_params
+      flash[:success] = "Question updated!"
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new questions_params
+    if @question.save
+      flash[:success] = "Question created!"
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def questions_params
+    params.require(:question).permit(:title, :body)
+  end
+
+  def set_question!
+    @question = Question.find params[:id]
+  end
+end
