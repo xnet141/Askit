d5cbd38f0ea5e3553d3d79da3ecdf8a5641b9277
Alex Erofeev <xnet141@gmail.com>
2025-01-13 15:02:14 +0300

    Lesson 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :questions do#, only: %i[index new edit create update destroy show]
-    resources :answers, only: %i[create destroy update edit]
+    resources :answers, except: %i[new show]
   end
 
   root 'pages#index'
